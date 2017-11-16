0b67d3df2b3ee98b9aeabc0ab0f0a298f2c9dcef
Eric Olszewski <eolszewski@gmail.com>
2017-11-16 09:26:34 -0600

    Add default testrpc host for development environment.

--- dapp/truffle.js
+++ dapp/truffle.js
@@ -8,7 +8,7 @@
 module.exports = {
   networks: {
     development: {
-      host: process.env.RPC_HOST,
+      host: process.env.RPC_HOST || 'localhost',
       port: 8545,
       network_id: "*" // Match any network id
     },
